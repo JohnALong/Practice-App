ffda0facc060641e9a2072e1054b7fcec70e8162
John Long <jlcomp45@gmail.com>
2019-12-08 09:37:36 -0600

    corrected file structure by creating components folder and moving files into it, completed initial card structure and page load

---
+++ src/components/practice/PracticeCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PracticeCard extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>Name: <span>First Card</span></h3>
+          <p>First Card Data</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PracticeCard;
